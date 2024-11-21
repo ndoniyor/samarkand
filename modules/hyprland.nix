03af3de92e3778a85b8e2c2d204329cb5b8beee9
Doniyor Nimatullo <ndoniyor123@gmail.com>
2024-11-21 11:13:18 -0500

    remove unused

--- modules/hyprland.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, hyprland, ... }: {
-  wayland.windowManager.hyprland = {
-    enable = true;
-    extraConfig = builtins.readFile ../dotfiles/hypr/hyprland.conf;
-  };
-  programs.kitty.enable = true;
-}
