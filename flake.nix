f7e47856ae92dee0eac12d50c2c1a6cbc2a77270
Doniyor Nimatullo <ndoniyor123@gmail.com>
2024-11-21 11:14:30 -0500

    add config to unstable

--- flake.nix
+++ flake.nix
@@ -9,7 +9,6 @@
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
   };
 
   outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
@@ -17,13 +16,18 @@
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+      pkgs-unstable = import nixpkgs-unstable {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
     in
     {
       nixosConfigurations = {
         nixos = lib.nixosSystem {
           inherit system;
-          specialArgs = { inherit pkgs-unstable; };
+          specialArgs = { inherit pkgs-unstable inputs; };
           modules = [ ./system/configuration.nix ];
         };
       };
