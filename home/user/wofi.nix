afa0c2afefb253e9dc6588e01afd09fdbbf6444c
Doniyor Nimatullo <ndoniyor123@gmail.com>
2024-11-25 21:03:59 -0500

    add styles

--- home/user/wofi.nix
+++ home/user/wofi.nix
@@ -1,21 +1,25 @@
 { config, lib, pkgs, ...}:
 {
-  programs.wofi.settings = {
-    mode = "drun,run";
+  programs.wofi = {
+    enable = true;
+    settings = {
+      mode = "drun,run";
 
-    width = "50%";
-    height = "40%";
-    location = "center";
-    prompt = "Search";
+      width = "50%";
+      height = "40%";
+      location = "center";
+      prompt = "Search";
 
-    matching = "fuzzy";
-    insenstive = true;
+      matching = "fuzzy";
+      insenstive = true;
 
-    allow_images = true;
-    gtk_dark = true;
+      allow_images = true;
+      gtk_dark = true;
 
-    "drun_display_generic" = true;
-    "run-always_parse_args" = true;
-    "run-show_all" = false;
+      "drun_display_generic" = true;
+      "run-always_parse_args" = true;
+      "run-show_all" = false;
+    };
+    style = builtins.readFile ./config/hypr/wofi/styles.css;
   };
 }
