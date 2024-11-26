eaa3e0bd274e913cb29dc0b198db49fd499736be
Doniyor Nimatullo <ndoniyor123@gmail.com>
2024-11-25 20:45:02 -0500

    add waybar changes

--- home/user/waybar.nix
+++ home/user/waybar.nix
@@ -8,46 +8,72 @@
         position = "top";
         height = 30;
 
-        modules-left = [ "hyprland/workspaces" ];
+        modules-left = [ "hyprland/window" "hyprland/workspaces" ];
         modules-center = [ ];
-        modules-right = [ "cpu" "memory" "network" "battery" "clock" ];
-
-        "hyprland/workspaces" = {
-          format = "{name}";
-          sort-by-number = true;
-        };
+        modules-right = [ "pulseaudio" "backlight" "cpu" "memory" "network" "battery" "clock" ];
 
         "clock" = {
           format = "{:%I:%M %p  %a, %b %d}";
           tooltip = false;
         };
         "battery" = {
-          format = "{capacity}% {icon}";
+          format = "{icon} {capacity}%";
           format-icons = [ "󰁺" "󰁼" "󰁽" "󰂀" "󰁹" ];
-          format-charging = "{capacity}% 󰢝 ";
-          format-plugged = "{capacity}% 󰂅 ";
+          format-charging = "󰢝 {capacity}%";
+          format-plugged = "󰂅 {capacity}%";
           states = {
             warning = 30;
             critical = 15;
           };
         };
         "network" = {
-          format-wifi = "{essid} {icon}";
+          format-wifi = "{icon} {essid}";
           format-icons = [ "󰤯" "󰤟" "󰤢" "󰤥" ];
-          format-ethernet = "Connected ";
-          format-disconnected = "Disconnected 󰤭";
+          format-ethernet = "Connected";
+          format-disconnected = "󰤭 Disconnected";
           tooltip-format = "{ifname}: {ipaddr}/{cidr}";
+          on-click = "kitty nmtui";
         };
         "cpu" = {
-          format = "{usage}%  ";
+          format = " {usage}%";
           interval = 10;
           tooltip = false;
         };
 
         "memory" = {
-          format = "{percentage}%  ";
+          format = "  {percentage}%";
           interval = 10;
           tooltip = false;
+        };
+
+        "backlight" = {
+          format = "{icon} {percent}%";
+          format-icons = [ "󰃟" ];
+        };
+
+        "hyprland/window" = {
+          format = "{class}";
+          rewrite = {
+            "firefox" = "";
+            "Code" = "󰨞";
+            "kitty" = "";
+            "org.kde.dolphin" = "";
+            "Spotify" = "";
+          };
+        };
+
+        "hyprland/workspaces" = {
+          format = "{name}";
+          sort-by-number = true;
+        };
+
+        "pulseaudio" = {
+          format = "{icon} {volume}%";
+          format-icons = {
+            default = ["" "" ""];
+          };
+          format-muted = " 0%";
+          on-click = "pavucontrol";
         };
       };
     };
