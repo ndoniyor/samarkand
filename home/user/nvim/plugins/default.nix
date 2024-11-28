2df75cd41f946fe23985f6af34a5d2aa0ca76c03
Doniyor Nimatullo <ndoniyor123@gmail.com>
2024-11-28 12:21:29 -0500

    added new plugins to default imports

--- home/user/nvim/plugins/default.nix
+++ home/user/nvim/plugins/default.nix
@@ -3,5 +3,13 @@
     imports = [
         ./treesitter.nix
         ./neo-tree.nix
+        ./cmp.nix
+        ./lualine.nix        
+        ./lazygit.nix
     ];
+    programs.nixvim = {
+        plugins = {
+            web-devicons.enable = true;
+        };
+    };
 }
